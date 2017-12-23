f8390b34649a02a61d245ac4958f609f0ea7f77a
SEJean <jeank714@gmail.com>
2017-12-23 00:54:22 -0500

    First

---
+++ CodeWars/CodeWars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+
+        /// <summary>
+        /// Challenge URL : https://www.codewars.com/kata/ip-validation/
+        /// </summary>
+        /// <param name="IpAddres"></param>
+        /// <returns>true if the IP address is valid</returns>
+        public static bool is_valid_IP(string IpAddres)
+        {
+            string[] numbers = IpAddres.Split('.');
+            bool ipValid = true;
+            ipValid = IpAddres != "" && string.Join("", numbers).All(char.IsDigit) && numbers.Length == 4;
+
+            for (int i = 0; i < numbers.Length && ipValid; i++)
+            {
+                int num = Convert.ToInt32(numbers[i]);
+                ipValid = (num >= 0 && num <= 255) && numbers[i][0] != '0';
+            }
+            return ipValid;
+        }
+    }
+}
